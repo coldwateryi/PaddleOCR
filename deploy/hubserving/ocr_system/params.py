9cf99f48407d6ccce236f557ef4a509ae408b178
coldwateryi <yijihu@hotmail.com>
2021-03-22 19:00:05 +0800

    Update params.py

--- deploy/hubserving/ocr_system/params.py
+++ deploy/hubserving/ocr_system/params.py
@@ -13,7 +13,7 @@ def read_params():
 
     #params for text detector
     cfg.det_algorithm = "DB"
-    cfg.det_model_dir = "./inference/ch_ppocr_server_v2.0_det_infer/"
+    cfg.det_model_dir = "/PaddleOCR/inference/ch_ppocr_server_v2.0_det_infer/"
     cfg.det_limit_side_len = 960
     cfg.det_limit_type = 'max'
 
@@ -30,19 +30,19 @@ def read_params():
 
     #params for text recognizer
     cfg.rec_algorithm = "CRNN"
-    cfg.rec_model_dir = "./inference/ch_ppocr_server_v2.0_rec_infer/"
+    cfg.rec_model_dir = "/PaddleOCR/inference//ch_ppocr_server_v2.0_rec_infer/"
 
     cfg.rec_image_shape = "3, 32, 320"
     cfg.rec_char_type = 'ch'
     cfg.rec_batch_num = 30
     cfg.max_text_length = 25
 
-    cfg.rec_char_dict_path = "./ppocr/utils/ppocr_keys_v1.txt"
+    cfg.rec_char_dict_path = "/PaddleOCR/ppocr/utils/ppocr_keys_v1.txt"
     cfg.use_space_char = True
 
     #params for text classifier
     cfg.use_angle_cls = True
-    cfg.cls_model_dir = "./inference/ch_ppocr_mobile_v2.0_cls_infer/"
+    cfg.cls_model_dir = "/PaddleOCR/inference/ch_ppocr_mobile_v2.0_cls_infer/"
     cfg.cls_image_shape = "3, 48, 192"
     cfg.label_list = ['0', '180']
     cfg.cls_batch_num = 30
